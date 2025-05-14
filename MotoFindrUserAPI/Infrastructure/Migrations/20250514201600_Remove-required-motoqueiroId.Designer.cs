a71663bbfdce41e95d7eedf9ef5a9b3e9441bd0d
victorcosmedev <victorcosme.profissional@gmail.com>
2025-05-14 17:38:03 -0300

    chore: Feita migration  de motoqueiroId aceita null

--- MotoFindrUserAPI/Infrastructure/Migrations/20250514201600_Remove-required-motoqueiroId.Designer.cs
+++ MotoFindrUserAPI/Infrastructure/Migrations/20250514201600_Remove-required-motoqueiroId.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MotoFindrUserAPI.Infrastructure.AppData;
 using Oracle.EntityFrameworkCore.Metadata;
@@ -11,9 +12,11 @@ using Oracle.EntityFrameworkCore.Metadata;
 namespace MotoFindrUserAPI.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20250514201600_Remove-required-motoqueiroId")]
+    partial class RemoverequiredmotoqueiroId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
