8aa78496eedb6b283b1d073f998631cf838c9485
victorcosmedev <victorcosme.profissional@gmail.com>
2025-08-27 19:45:44 -0300

    feat: added EnderecoEntity.

---
+++ MotoFindrUserAPI/Domain/Entities/Endereco.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MotoFindrUserAPI.Domain.Entities
+{
+    [Table("tb_mf_endereco")]
+    public class Endereco
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Logradouro { get; set; } = string.Empty;
+        [Required]
+        public string Complemento { get; set; } = string.Empty;
+        [Required]
+        public string Uf { get; set; } = string.Empty;
+        [Required]
+        public string Numero { get; set; } = string.Empty;
+        [Required]
+        public string Localidade { get; set; } = string.Empty;
+    }
+}
