80e56a4e181a0cf934e2fde05a4045da50eb12f2
victorcosmedev <victorcosme.profissional@gmail.com>
2025-09-20 14:39:36 -0300

    fix

--- MotoFindrUserAPI/Utils/Samples/Endereco/EnderecoResponseListSample.cs
+++ MotoFindrUserAPI/Utils/Samples/Endereco/EnderecoResponseListSample.cs
@@ -8,27 +8,28 @@ namespace MotoFindrUserAPI.Utils.Samples.Endereco
         public IEnumerable<EnderecoDTO> GetExamples()
         {
             return new List<EnderecoDTO>
-        {
-            new EnderecoDTO {
-                Id = 1,
-                Logradouro = "Av. Brasil",
-                Numero = "10",
-                Complemento = "Apto 101",
-                Uf = "RJ",
-                Municipio = "Niterói",
-                Cep = "24020000",
-                MotoqueiroId = 1
-            },
-            new EnderecoDTO {
-                Id = 2,
-                Logradouro = "Rua das Palmeiras",
-                Numero = "20",
-                Complemento = "Casa",
-                Uf = "BA",
-                Municipio = "Salvador",
-                Cep = "40010000",
-                MotoqueiroId = 2
-            }
-        };
+            {
+                new EnderecoDTO {
+                    Id = 1,
+                    Logradouro = "Av. Brasil",
+                    Numero = "10",
+                    Complemento = "Apto 101",
+                    Uf = "RJ",
+                    Municipio = "Niterói",
+                    Cep = "24020000",
+                    MotoqueiroId = 1
+                },
+                new EnderecoDTO {
+                    Id = 2,
+                    Logradouro = "Rua das Palmeiras",
+                    Numero = "20",
+                    Complemento = "Casa",
+                    Uf = "BA",
+                    Municipio = "Salvador",
+                    Cep = "40010000",
+                    MotoqueiroId = 2
+                }
+            };
         }
     }
+}
