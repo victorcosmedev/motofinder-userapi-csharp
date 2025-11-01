0394fc6b111ff52cebb60ac2706b87ae2bfbc5a0
victorcosmedev <victorcosme.profissional@gmail.com>
2025-11-01 00:29:33 -0300

    chore: copiando arquivos do application e adicionando referencia à domain.

---
+++ MotoFindrUserAPI.Application/DTOs/MotoDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MotoFindrUserAPI.Application.DTOs
+{
+    public class MotoDto
+    {
+        public int Id { get; set; }
+        [StringLength(100, MinimumLength = 2, ErrorMessage = "O modelo deve ter entre 2 e 100 caracteres")]
+        public string Modelo { get; set; } = string.Empty;
+        [Required]
+        [Range(1900, 2100, ErrorMessage = "O ano deve estar entre 1900 e 2100")]
+        public int AnoDeFabricacao { get; set; }
+        [RegularExpression(@"^[A-Za-z0-9]{17}$",
+                ErrorMessage = "Chassi inválido (deve ter 17 caracteres alfanuméricos)")]
+        public string Chassi { get; set; } = string.Empty;
+        [StringLength(7, MinimumLength = 7, ErrorMessage = "A placa deve ter 7 caracteres.")]
+        public string Placa { get; set; } = string.Empty;
+        public int? MotoqueiroId { get; set; }
+    }
+}
---
+++ MotoFindrUserAPI.Application/Services/MotoqueiroApplicationService.cs
@@ -0,0 +1,110 @@
+﻿using AutoMapper;
+using MotoFindrUserAPI.Application.DTOs;
+using MotoFindrUserAPI.Application.Interfaces;
+using MotoFindrUserAPI.Domain.Entities;
+using MotoFindrUserAPI.Domain.Interfaces;
+using MotoFindrUserAPI.Models.PageResultModel;
+
+namespace MotoFindrUserAPI.Application.Services
+{
+    public class MotoqueiroApplicationService : IMotoqueiroApplicationService
+    {
+        private readonly IMotoqueiroRepository _motoqueiroRepository;
+        private readonly IMotoRepository _motoRepository;
+        private readonly IMapper _mapper;
+
+        public MotoqueiroApplicationService(IMotoqueiroRepository motoqueiroRepository, IMotoRepository motoRepository, IMapper mapper)
+        {
+            _motoqueiroRepository = motoqueiroRepository;
+            _motoRepository = motoRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<MotoqueiroDto?> ObterPorIdAsync(int id)
+        {
+            var entity = await _motoqueiroRepository.BuscarPorIdAsync(id);
+            return _mapper.Map<MotoqueiroDto>(entity);
+        }
+
+        public async Task<MotoqueiroDto?> ObterPorCpfAsync(string cpf)
+        {
+            var entity = await _motoqueiroRepository.BuscarPorCpfAsync(cpf);
+            return _mapper.Map<MotoqueiroDto>(entity);
+        }
+
+        public async Task<MotoqueiroDto> CriarAsync(MotoqueiroDto motoqueiro)
+        {
+            MotoEntity? moto = null;
+            var entity = _mapper.Map<MotoqueiroEntity>(motoqueiro);
+            if (motoqueiro.MotoId.HasValue && motoqueiro.MotoId != 0)
+            {
+                moto = await AtribuirEValidarMotoAsync(motoqueiro.MotoId.Value, entity);
+            }
+            else
+            {
+                entity.MotoId = null;
+            }
+
+            entity.Moto = moto;
+
+            entity = await _motoqueiroRepository.SalvarAsync(entity);
+            return _mapper.Map<MotoqueiroDto>(entity);
+        }
+
+        public async Task<bool> AtualizarAsync(int id, MotoqueiroDto motoqueiro)
+        {
+            MotoEntity? moto = null;
+            var entity = _mapper.Map<MotoqueiroEntity>(motoqueiro);
+            if (motoqueiro.MotoId.HasValue && motoqueiro.MotoId != 0)
+            {
+                moto = await AtribuirEValidarMotoAsync(motoqueiro.MotoId.Value, entity);
+            } else
+            {
+                entity.MotoId = null;
+            }
+
+            entity.Moto = moto;
+            
+            return await _motoqueiroRepository.AtualizarAsync(id, entity);
+        }
+
+        public async Task<bool> RemoverAsync(int id)
+        {
+            return await _motoqueiroRepository.DeletarAsync(id);
+        }
+
+        private async Task<MotoEntity?> AtribuirEValidarMotoAsync(int motoId, MotoqueiroEntity motoqueiroEntity)
+        {
+            var moto = await _motoRepository.BuscarPorIdAsync(motoId);
+            if (moto == null)
+                throw new Exception("Moto não encontrada");
+
+            if (moto.Motoqueiro != null && moto.Motoqueiro.Id != motoId)
+                throw new Exception("Esta moto já está associada a outro motoqueiro.");
+
+            moto.Motoqueiro = motoqueiroEntity;
+            moto.MotoqueiroId = motoqueiroEntity.Id;
+
+            await _motoRepository.AtualizarAsync(motoId, moto);
+            return moto;
+        }
+
+        public async Task<PageResultModel<IEnumerable<MotoqueiroDto?>>> ObterTodos(int pageNumber = 1, int pageSize = 10)
+        {
+            var pageResult = await _motoqueiroRepository.BuscarTodos(pageNumber, pageSize);
+
+            var dtos = pageResult.Items.Select(x => _mapper.Map<MotoqueiroDto>(x));
+
+            var pageResultDto = new PageResultModel<IEnumerable<MotoqueiroDto?>>
+            {
+                Items = dtos,
+                TotalItens = pageResult.TotalItens,
+                NumeroPagina = pageResult.NumeroPagina,
+                TamanhoPagina = pageResult.TamanhoPagina
+            };
+            return pageResultDto;
+        }
+    }
+
+}
+
