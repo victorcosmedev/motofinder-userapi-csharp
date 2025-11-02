a3d40db4adcfff09ae28f0719443ca7cff9cb395
victorcosmedev <victorcosme.profissional@gmail.com>
2025-11-01 23:26:51 -0300

    chore: removendo models da application.

--- MotoFindrUserAPI.Application/Models/Hateoas/HateoasResponse.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MotoFindrUserAPI.Application.Models.Hateoas
-{
-    public class HateoasResponse<T>
-    {
-        public string? Message { get; set; }
-        public T? Data { get; set; }
-        public List<LinkDto> Links { get; set; }
-    }
-}
