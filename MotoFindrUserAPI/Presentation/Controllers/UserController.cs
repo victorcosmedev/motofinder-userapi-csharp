5cd047b3c6233bbbaa6285a89b2f489d4bb91c74
victorcosmedev <victorcosme.profissional@gmail.com>
2025-11-02 13:21:06 -0300

    feat: ajustes no controller para o JWT

--- MotoFindrUserAPI/Presentation/Controllers/UserController.cs
+++ MotoFindrUserAPI/Presentation/Controllers/UserController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using MotoFindrUserAPI.Application.DTOs;
 using MotoFindrUserAPI.Application.Interfaces;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace MotoFindrUserAPI.Presentation.Controllers;
 
@@ -8,10 +12,13 @@ namespace MotoFindrUserAPI.Presentation.Controllers;
 [Route("api/[controller]")]
 public class UserController : ControllerBase
 {
-    private readonly IUserApplicationService _authService;
-    public UserController(IUserApplicationService authService)
+    private readonly IUserApplicationService _userService;
+    private readonly IConfiguration _configuration;
+
+    public UserController(IUserApplicationService userService, IConfiguration configuration)
     {
-        _authService = authService;
+        _userService = userService;
+        _configuration = configuration;
     }
 
     [HttpPost]
@@ -19,7 +26,7 @@ public class UserController : ControllerBase
     {
         try
         {
-            await _authService.RegisterAsync(request);
+            await _userService.RegisterAsync(request);
             return Ok(new { message = "Usuário criado com sucesso!" });
         }
         catch (InvalidOperationException ex)
@@ -35,15 +42,34 @@ public class UserController : ControllerBase
     }
 
     [HttpPost]
-    public async Task<IActionResult> Login([FromBody] UserLoginDto request)
+    public async Task<IActionResult> Login([FromBody] UserDto request)
     {
-        var token = await _authService.LoginAsync(request);
+        var result = await _userService.AuthenticateAsync(request);
 
-        if (token == null)
+        if(!result.IsSuccess)
         {
-            return Unauthorized("Usuário ou senha inválidos.");
+            return StatusCode(result.StatusCode, result.Error);
         }
 
-        return Ok(new { token });
+        var tokenHandler = new JwtSecurityTokenHandler();
+
+        var key = Encoding.ASCII.GetBytes(_configuration["Jtw:Key"]!.ToString());
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Expires = DateTime.UtcNow.AddHours(8),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature),
+            Subject = new ClaimsIdentity(new Claim[] {
+                new Claim(ClaimTypes.Name, result.Value!.Username.ToString()),
+            })
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return StatusCode(result.StatusCode, new
+        {
+            user = result.Value.Username,
+            token = tokenHandler.WriteToken(token),
+        });
     }
 }
