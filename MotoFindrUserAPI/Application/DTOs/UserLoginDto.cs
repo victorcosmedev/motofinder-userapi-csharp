c2e7706cf7bfdf709765467d000ce3ce036d37a0
victorcosmedev <victorcosme.profissional@gmail.com>
2025-11-01 22:54:48 -0300

    chore: removendo arquivos do projeto web.

--- MotoFindrUserAPI/Application/DTOs/UserLoginDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MotoFindrUserAPI.Application.DTOs
-{
-    public class UserLoginDto
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
