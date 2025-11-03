038ab2bbf45427cca6cc09ebda5b7c07a650a820
victorcosmedev <victorcosme.profissional@gmail.com>
2025-11-03 00:00:51 -0300

    criando ConfigureSwaggerOptions

---
+++ MotoFindrUserAPI/Utils/ConfigureSwaggerOptions.cs
@@ -0,0 +1,42 @@
+﻿namespace MotoFindrUserAPI.Utils
+{
+    using Asp.Versioning.ApiExplorer;
+    using Microsoft.Extensions.Options;
+    using Microsoft.OpenApi.Models;
+    using Swashbuckle.AspNetCore.SwaggerGen;
+
+    namespace MotoFindrUserAPI.Utils
+    {
+        public class ConfigureSwaggerOptions : IConfigureOptions<SwaggerGenOptions>
+        {
+            private readonly IApiVersionDescriptionProvider _provider;
+            public ConfigureSwaggerOptions(IApiVersionDescriptionProvider provider)
+            {
+                _provider = provider;
+            }
+            public void Configure(SwaggerGenOptions options)
+            {
+                foreach (var description in _provider.ApiVersionDescriptions)
+                {
+                    options.SwaggerDoc(description.GroupName, CreateInfoForApiVersion(description));
+                }
+            }
+
+            private static OpenApiInfo CreateInfoForApiVersion(ApiVersionDescription description)
+            {
+                var info = new OpenApiInfo()
+                {
+                    Title = "MotoFindr User API",
+                    Version = description.ApiVersion.ToString(),
+                    Description = "API para gerenciamento de usuários do MotoFindr."
+                };
+
+                if (description.IsDeprecated)
+                {
+                    info.Description += " Esta versão da API está obsoleta.";
+                }
+
+                return info;
+            }
+        }
+    }
