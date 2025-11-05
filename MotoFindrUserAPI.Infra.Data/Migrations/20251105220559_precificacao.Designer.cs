a852f405360c2008b577a839a4319039a86e3967
victorcosmedev <victorcosme.profissional@gmail.com>
2025-11-05 19:23:19 -0300

    migration precificacao

--- MotoFindrUserAPI.Infra.Data/Migrations/20251105220559_precificacao.Designer.cs
+++ MotoFindrUserAPI.Infra.Data/Migrations/20251105220559_precificacao.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MotoFindrUserAPI.Infra.Data.AppData;
 using Oracle.EntityFrameworkCore.Metadata;
@@ -11,9 +12,11 @@ using Oracle.EntityFrameworkCore.Metadata;
 namespace MotoFindrUserAPI.Infra.Data.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20251105220559_precificacao")]
+    partial class precificacao
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
