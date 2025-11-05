0805a0cee4308ac4ee7e7a811d8fec599b99d406
victorcosmedev <victorcosme.profissional@gmail.com>
2025-11-04 22:35:12 -0300

    feat: criação do repository

---
+++ MotoFindrUserAPI.Domain/Interfaces/IPrecificacaoMotoRepository.cs
@@ -0,0 +1,16 @@
+﻿using MotoFindrUserAPI.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoFindrUserAPI.Domain.Interfaces
+{
+    public interface IPrecificacaoMotoRepository
+    {
+        Task<PrecificacaoMotoEntity> SalvarOuAtualizarAsync(PrecificacaoMotoEntity entity);
+        PrecificacaoMotoEntity? ObterPorMotoId(int motoId);
+        IEnumerable<PrecificacaoMotoEntity> ObterDadosParaTreinamento();
+    }
+}
---
+++ MotoFindrUserAPI.Infra.Data/Repositories/PrecificacaoMotoRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotoFindrUserAPI.Domain.Entities;
+using MotoFindrUserAPI.Infra.Data.AppData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotoFindrUserAPI.Infra.Data.Repositories
+{
+    public class PrecificacaoMotoRepository
+    {
+        private readonly ApplicationContext _context;
+
+        public PrecificacaoMotoRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<PrecificacaoMotoEntity> SalvarOuAtualizarAsync(PrecificacaoMotoEntity entity)
+        {
+            var precificacaoExistente = await _context.PrecificacaoMotos
+                .FirstOrDefaultAsync(p => p.MotoId == entity.MotoId);
+
+            if (precificacaoExistente != null)
+            {
+                precificacaoExistente.Preco = entity.Preco;
+                entity = precificacaoExistente;
+            }
+            else
+            {
+                await _context.PrecificacaoMotos.AddAsync(entity);
+            }
+
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public PrecificacaoMotoEntity? ObterPorMotoId(int motoId)
+        {
+            return _context.PrecificacaoMotos
+                .FirstOrDefault(p => p.MotoId == motoId);
+        }
+
+        public IEnumerable<PrecificacaoMotoEntity> ObterDadosParaTreinamento()
+        {
+            return _context.PrecificacaoMotos
+                .Include(p => p.Moto)
+                .AsNoTracking()
+                .ToList();
+        }
+    }
+}
