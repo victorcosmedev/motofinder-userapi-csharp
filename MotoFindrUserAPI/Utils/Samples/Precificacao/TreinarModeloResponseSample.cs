2ea65577eead2aebdd830a96aa78b70bbdfa34c9
victorcosmedev <victorcosme.profissional@gmail.com>
2025-11-04 23:08:25 -0300

    response samples.

---
+++ MotoFindrUserAPI/Utils/Samples/Precificacao/TreinarModeloResponseSample.cs
@@ -0,0 +1,16 @@
+﻿using MotoFindrUserAPI.Application.DTOs;
+using Swashbuckle.AspNetCore.Filters;
+
+namespace MotoFindrUserAPI.Utils.Samples.Precificacao
+{
+    public class TreinarModeloResponseSample : IExamplesProvider<MessageResponseDto>
+    {
+        public MessageResponseDto GetExamples()
+        {
+            return new MessageResponseDto
+            {
+                Message = "Modelo treinado."
+            };
+        }
+    }
+}
